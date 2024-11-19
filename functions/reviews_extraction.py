b214229f85b0265d679da25db3ea884914179ab4
Diógenes Falcade <77336996+diogenesfalcade@users.noreply.github.com>
2024-11-19 16:27:47 -0300

    Create reviews_extraction.py

---
+++ functions/reviews_extraction.py
@@ -0,0 +1,9 @@
+import googlemaps
+import time
+import requests
+import json
+import pandas as pd
+from textblob import TextBlob
+import nltk
+from nltk.sentiment import SentimentIntensityAnalyzer
+
